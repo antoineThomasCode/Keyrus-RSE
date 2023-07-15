9576526b20faec7bb9dcbbfac0eb67a0605b2495
Antoine Thomas <antoine@coyali.com>
2023-07-15 18:44:46 +0200

    added first section mobile + desktop and tablet

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ document.addEventListener('DOMContentLoaded', function() {
     h1.classList.add('appear');
   }, 800);
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+});
+
+
+// sliders from Swipper.js Déclarations 
+
+// slider strategy section 
+
